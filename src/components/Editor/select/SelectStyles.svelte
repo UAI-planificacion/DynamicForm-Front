a849dacfea29b1201628a2247cdefc5a11a531e4
kevinkeyssx <keyss1700@gmail.com>
2025-04-04 21:04:54 -0300

    Select Styles

--- src/components/Editor/select/SelectStyles.svelte
+++ src/components/Editor/select/SelectStyles.svelte
@@ -6,18 +6,26 @@
     import { styles }                       from "$lib";
 
     export let shapeInput: ShapeInput;
+
+    const shape = styles[shapeInput.shape || 'none'] as InputStyle;
+
+    shapeInput.boxSelectClass       = shapeInput.boxSelectClass     ?? shape.box;
+    shapeInput.contentSelectClass   = shapeInput.contentSelectClass ?? shape.content;
+    shapeInput.itemSelectClass      = shapeInput.itemSelectClass    ?? shape.item;
+    shapeInput.groupSelectClass     = shapeInput.groupSelectClass   ?? shape.group;
+    shapeInput.inputSelectClass     = shapeInput.inputSelectClass   ?? shape.input;
 </script>
 
 <TextArea
     shapeInput = {{
         id          : uuid(),
         name        : 'class',
-        label       : 'Componente principal box',
+        label       : 'Estilos de la caja',
         placeholder : 'Ingrese los estilos de la caja',
-        rows        : 3,
-        value       : shapeInput.class_ ?? ( styles[shapeInput.shape || 'none'] as InputStyle ).box
+        rows        : 4,
     }}
-    onInput = {( event: Event ) => shapeInput.class_ = ( event.target as HTMLInputElement ).value }
+    onInput = {( event: Event ) => shapeInput.boxSelectClass = ( event.target as HTMLInputElement ).value }
+    value = { shapeInput.boxSelectClass }
 />
 
 <TextArea
@@ -27,9 +35,9 @@
         label       : 'Estilos del contenido',
         placeholder : 'Ingrese los estilos del contenido',
         rows        : 2,
-        value       : shapeInput.contentStyle ?? ( styles[shapeInput.shape || 'none'] as InputStyle ).content
     }}
-    onInput = {( event: Event ) => shapeInput.contentStyle = ( event.target as HTMLInputElement ).value }
+    onInput = {( event: Event ) => shapeInput.contentSelectClass = ( event.target as HTMLInputElement ).value }
+    value = { shapeInput.contentSelectClass }
 />
 
 <TextArea
@@ -38,8 +46,32 @@
         name        : 'class',
         label       : 'Estilos de cada item',
         placeholder : 'Ingrese los estilos de cada item',
-        rows        : 2,
-        value       : shapeInput.itemStyle ?? ( styles[shapeInput.shape || 'none'] as InputStyle ).item
+        rows        : 4,
     }}
-    onInput = {( event: Event ) => shapeInput.itemStyle = ( event.target as HTMLInputElement ).value }
+    onInput = {( event: Event ) => shapeInput.itemSelectClass = ( event.target as HTMLInputElement ).value }
+    value = { shapeInput.itemSelectClass }
+/>
+
+<TextArea
+    shapeInput = {{
+        id          : uuid(),
+        name        : 'class',
+        label       : 'Estilos del grupo',
+        placeholder : 'Ingrese los estilos del grupo',
+        rows        : 3,
+    }}
+    onInput = {( event: Event ) => shapeInput.groupSelectClass = ( event.target as HTMLInputElement ).value }
+    value = { shapeInput.groupSelectClass }
+/>
+
+<TextArea
+    shapeInput = {{
+        id          : uuid(),
+        name        : 'class',
+        label       : 'Estilos del input',
+        placeholder : 'Ingrese los estilos del input',
+        rows        : 4,
+    }}
+    onInput = {( event: Event ) => shapeInput.inputSelectClass = ( event.target as HTMLInputElement ).value }
+    value = { shapeInput.inputSelectClass }
 />
