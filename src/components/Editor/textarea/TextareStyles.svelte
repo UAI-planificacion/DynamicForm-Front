11824e6ba70382ec642017bcb3ec59152bc2e51b
kevinkeyssx <keyss1700@gmail.com>
2025-04-07 17:48:35 -0400

    TextArea Styles component added

---
+++ src/components/Editor/textarea/TextareStyles.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import { v4 as uuid } from 'uuid';
+
+    import { TextArea } from "$components";
+    import type { ShapeInput } from "$models";
+    import { styles } from "$lib";
+
+    export let shapeInput: ShapeInput;
+</script>
+
+<TextArea
+    onInput     = {( value ) => shapeInput.textareaClass = value }
+    value       = { shapeInput.textareaClass ?? ( styles.textarea as string )}
+    shapeInput  = {{
+        id          : uuid(),
+        name        : 'class',
+        label       : 'Estilos con tailwind',
+        placeholder : 'Ingrese los estilos',
+        rows        : 4,
+    }}
+/>
