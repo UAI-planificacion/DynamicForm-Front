b2fa8e1e8e26050e6ec4d7b98a5b3ee9ed7a514c
kevinkeyssx <keyss1700@gmail.com>
2025-04-07 17:47:44 -0400

    Button Styles component added

---
+++ src/components/Editor/buttons/ButtonStyles.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import { v4 as uuid } from 'uuid';
+
+    import { TextArea } from "$components";
+    import type { ShapeInput } from "$models";
+    import { styles } from "$lib";
+
+    export let shapeInput: ShapeInput;
+</script>
+
+<TextArea
+    onInput     = {( value ) => shapeInput.buttonClass = value }
+    value       = { shapeInput.buttonClass ?? ( styles.button as string )}
+    shapeInput  = {{
+        id          : uuid(),
+        name        : 'class',
+        label       : 'Estilos con tailwind',
+        placeholder : 'Ingrese los estilos',
+        rows        : 4,
+    }}
+/>
