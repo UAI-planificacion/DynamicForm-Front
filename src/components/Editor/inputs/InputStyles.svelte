5c4678652e061b188e0f6dce5197532344387420
kevinkeyssx <keyss1700@gmail.com>
2025-04-07 17:47:55 -0400

    Input Styles component added

---
+++ src/components/Editor/inputs/InputStyles.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import { v4 as uuid } from 'uuid';
+
+    import { TextArea } from "$components";
+    import type { ShapeInput } from "$models";
+    import { styles } from "$lib";
+
+    export let shapeInput: ShapeInput;
+</script>
+
+<TextArea
+    onInput     = {( value ) => shapeInput.inputClass = value }
+    value       = { shapeInput.inputClass ?? ( styles.input as string ) }
+    shapeInput  = {{
+        id          : uuid(),
+        name        : 'class',
+        label       : 'Estilos con tailwind',
+        placeholder : 'Ingrese los estilos',
+        rows        : 4,
+    }}
+/>
