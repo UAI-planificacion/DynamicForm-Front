f2e436ece7a0de1f5a774f82f6ce2021377088b1
kevinkeyssx <keyss1700@gmail.com>
2025-04-08 21:46:34 -0400

    Methos is undefined

--- src/lib/button-template.ts
+++ src/lib/button-template.ts
@@ -1,7 +1,6 @@
 import { v4 as uuid }	from 'uuid';
 
 import type { ShapeInput }  from "$models";
-import { METHOD }           from "./http-codes";
 
 
 export const buttonTemplate: ShapeInput = {
@@ -9,7 +8,7 @@ export const buttonTemplate: ShapeInput = {
     shape     		    : "button",
     name      		    : "Send",
     label     		    : "Enviar formulario",
-    method    		    : METHOD.GET,
+    method    		    : undefined,
     externalErrorMsg	: "Error de conexión",
     invalidErrorMsg	    : "Formulario inválido",
     httpList 			: [
