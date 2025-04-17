4e86f5b5e3892e9fb593f5d3b725d02aa99472b7
kevinkeyssx <keyss1700@gmail.com>
2025-04-17 19:12:38 -0400

    Loader color primary

--- src/components/Placeholder/CircleLoader.svelte
+++ src/components/Placeholder/CircleLoader.svelte
@@ -41,7 +41,7 @@
 .circle {
   width: 20px;
   height: 20px;
-  background-color: rgb(11, 140, 245);
+  background-color: #0060ff;
   border-radius: 50%;
   margin: 2px;
   animation: bounce 1.2s infinite ease-in-out;
