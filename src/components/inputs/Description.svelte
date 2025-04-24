27eeb925c205a8b89369e222e1504f7e9ce7045c
kevinkeyssx <keyss1700@gmail.com>
2025-04-24 18:59:48 -0400

    The error and description are displayed at the same time

--- src/components/inputs/Description.svelte
+++ src/components/inputs/Description.svelte
@@ -22,28 +22,32 @@
     export let date         : DateValue | DateRange | undefined = undefined;
 </script>
 
-
 {#if !shapeInput.valid && shapeInput.shape !== 'button'}
-    <span class="text-sm text-red-500 font-semibold">
+    <div class="flex items-center justify-between">
+        <span class="text-sm text-red-500 font-semibold">
+            {#if shapeInput.shape === 'input' || shapeInput.shape === 'textarea' || shapeInput.shape === 'markdown'}
+                { showErrorInput( shapeInput, value as string )}
+            {:else if shapeInput.shape === 'select'}
+                { showErrorSelect( shapeInput, value as Selected )}
+            {:else if shapeInput.shape === 'check'}
+                { showErrorCheck( shapeInput, checked )}
+            {:else if shapeInput.shape === 'datepicker'}
+                { showErrorDatePicker( shapeInput, date )}
+            {:else if shapeInput.shape === 'timer'}
+                { showErrorTimer( shapeInput, value as string )}
+            {/if}
+        </span>
+
         {#if shapeInput.shape === 'input' || shapeInput.shape === 'textarea' || shapeInput.shape === 'markdown'}
-            { showErrorInput( shapeInput, value as string )}
-        {:else if shapeInput.shape === 'select'}
-            { showErrorSelect( shapeInput, value as Selected )}
-        {:else if shapeInput.shape === 'check'}
-            { showErrorCheck( shapeInput, checked )}
-        {:else if shapeInput.shape === 'datepicker'}
-            { showErrorDatePicker( shapeInput, date )}
-        {:else if shapeInput.shape === 'timer'}
-            { showErrorTimer( shapeInput, value as string )}
+            <span class="text-sm text-gray-500">
+                { (value as string)?.length }/{ shapeInput?.maxLength }
+            </span>
         {/if}
-    </span>
-{:else if shapeInput.description}
+    </div>
+{/if}
+
+{#if shapeInput.description}
     <span class="text-sm text-zinc-500/90 dark:text-zinc-400 font-semibold">
         { shapeInput?.description }
     </span>
 {/if}
-
-<!-- <span class="text-sm text-gray-500">
-        { value?.length }/{ shapeInput?.maxLength }
-    </span>
--->
