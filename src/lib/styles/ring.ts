f2a9f79a58a9df0f86c380ec2476c2429b80fbc6
kevinkeyssx <keyss1700@gmail.com>
2025-04-29 21:45:26 -0400

    Ring const added

---
+++ src/lib/styles/ring.ts
@@ -0,0 +1,9 @@
+import type { ShapeOption } from "$models";
+
+export const RING: ShapeOption[] = [
+    { label: "Ninguno", value: "0px" },
+    { label: "Pequeño", value: "1px" },
+    { label: "Mediano", value: "2px" },
+    { label: "Grande", value: "4px" },
+    { label: "Extra Grande", value: "8px" },
+];
