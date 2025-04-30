e88de1d7950bbdb95798c82aeaebe676eadd5121
kevinkeyssx <keyss1700@gmail.com>
2025-04-29 21:45:01 -0400

    Border const added

---
+++ src/lib/styles/border.ts
@@ -0,0 +1,8 @@
+import type { ShapeOption } from "$models";
+
+export const BORDER: ShapeOption[] = [
+    { label: "Ninguno", value: "none" },
+    { label: "Pequeño", value: "2" },
+    { label: "Mediano", value: "4" },
+    { label: "Grande", value: "8" },
+];
