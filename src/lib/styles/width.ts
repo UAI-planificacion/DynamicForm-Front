1da066664c129d65be191b9c52c915a7290ca24c
kevinkeyssx <keyss1700@gmail.com>
2025-04-29 21:46:04 -0400

    Width const added

---
+++ src/lib/styles/width.ts
@@ -0,0 +1,7 @@
+import type { ShapeOption } from "$models";
+
+export const WIDTH: ShapeOption[] = [
+    { label: "Pequeño", value: "7" },
+    { label: "Medio", value: "9" },
+    { label: "Grande", value: "12" },
+]
