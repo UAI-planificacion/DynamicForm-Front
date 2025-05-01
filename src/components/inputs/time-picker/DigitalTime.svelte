70cb034428b43851d0df93307e9a16efd638f370
kevinkeyssx <keyss1700@gmail.com>
2025-04-30 20:43:19 -0400

    Box styles implemented

--- src/components/inputs/time-picker/DigitalTime.svelte
+++ src/components/inputs/time-picker/DigitalTime.svelte
@@ -10,12 +10,26 @@
     import { Input }    from '$components';
     import { styles }   from '$lib';
     import Info         from '../Info.svelte';
+    import { theme }            from "$stores";
+    import { inputUAITheme } from '$lib/styles/themes/uai-theme';
+
 
 
     export let shapeInput   : ShapeInput;
     export let value        : Time | undefined = undefined;
     export let onTimerInput : ( value: string ) => void;
     export let setError     : VoidFunction = () => {};
+
+    shapeInput.inputStyle ??= inputUAITheme;
+
+    let isDarkMode = $theme === 'dark';
+
+
+    $: if ( $theme === 'dark' ) {
+        isDarkMode = true;
+    } else {
+        isDarkMode = false;
+    }
 
     let firstRender = true;
     let open = false;
@@ -124,14 +138,57 @@
 <Info { shapeInput } { value } { onTimerInput }>
 
 <div class="relative w-full" id={ shapeInput.id }>
+    <!-- class           = { `${( styles.digital as InputStyle ).box }` } -->
     <button
         disabled        = { shapeInput.disabled }
         type            = "button"
         id              = { shapeInput.id }
-        class           = { `${( styles.digital as InputStyle ).box }` }
         aria-haspopup   = "true"
         aria-expanded   = { open }
         on:click        = { togglePicker }
+        class       = {`px-3 py-2 transition-all duration-150 ease-in-out w-full flex items-center justify-between 
+            ${ shapeInput.inputStyle?.fontSize      ?? 'text-sm' }
+            ${ shapeInput.inputStyle?.height        ?? '' }
+            ${ shapeInput.inputStyle?.borderRadius  ?? 'rounded-md' }
+            ${ shapeInput.inputStyle?.borderSize    ?? 'border-0' }
+            ${ shapeInput.inputStyle?.boxShadow     ?? 'shadow-sm' }`
+        }
+        style={ `background-color: ${
+            isDarkMode
+                ? shapeInput.disabled || shapeInput.readonly
+                    ? shapeInput.inputStyle?.dark?.event?.disabled?.background ?? 'transparent'
+                    : shapeInput.inputStyle?.dark?.background   ?? 'transparent'
+                : shapeInput.disabled || shapeInput.readonly
+                    ? shapeInput.inputStyle?.light?.event?.disabled?.background ?? 'transparent'
+                    : shapeInput.inputStyle?.light?.background  ?? 'transparent'
+            }; color: ${
+                isDarkMode
+                    ? shapeInput.disabled || shapeInput.readonly
+                        ? shapeInput.inputStyle?.dark?.event?.disabled?.color ?? '#71717a'
+                        :shapeInput.inputStyle?.dark?.color    ?? '#d1d5db'
+                    : shapeInput.disabled || shapeInput.readonly
+                        ? shapeInput.inputStyle?.light?.event?.disabled?.color ?? '#71717a'
+                        :shapeInput.inputStyle?.light?.color   ?? 'black'
+            }; box-shadow: 0 0 0 ${shapeInput.inputStyle?.ringSize ?? '1px'} ${
+                isDarkMode
+                    ? shapeInput.inputStyle?.dark?.ring ?? '#3f3f46'  // zinc-700
+                    : shapeInput.inputStyle?.light?.ring ?? '#d4d4d8' // zinc-300
+            };`
+        }
+        on:focus={(e) => {
+            e.currentTarget.style.boxShadow = `0 0 0 ${shapeInput.inputStyle?.[isDarkMode ? 'dark' : 'light']?.event?.focus?.ringSize ?? '2px'} ${
+                isDarkMode
+                    ? shapeInput.inputStyle?.dark?.event?.focus?.ring ?? '#71717a' // zinc-500
+                    : shapeInput.inputStyle?.light?.event?.focus?.ring ?? '#a1a1aa' // zinc-400
+            }`;
+        }}
+        on:blur={(e) => {
+            e.currentTarget.style.boxShadow = `0 0 0 ${shapeInput.inputStyle?.ringSize ?? '1px'} ${
+                isDarkMode
+                    ? shapeInput.inputStyle?.dark?.ring ?? '#3f3f46' // zinc-500
+                    : shapeInput.inputStyle?.light?.ring ?? '#d4d4d8' // zinc-400
+            }`;
+        }}
     >
         {
             selectedHour === undefined ? 'hh' : selectedHour.toString().padStart(2, '0')
