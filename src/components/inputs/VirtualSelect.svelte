00d43cb265022c3f0f04a4e2b8d1d800feafb6b5
kevinkeyssx <keyss1700@gmail.com>
2025-04-30 20:43:09 -0400

    Box styles implemented

--- src/components/inputs/VirtualSelect.svelte
+++ src/components/inputs/VirtualSelect.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
     import { onMount } from 'svelte';
 
+    import { theme }            from "$stores";
+
+
     import { ChevronDown, Check, X } from 'lucide-svelte';
 
     import type {
@@ -15,6 +18,17 @@
     import { Input }    from '$components';
     import { styles }   from '$lib';
     import Info         from './Info.svelte';
+    import { inputUAITheme } from '$lib/styles/themes/uai-theme';
+
+
+    let isDarkMode = $theme === 'dark';
+
+
+$: if ( $theme === 'dark' ) {
+    isDarkMode = true;
+} else {
+    isDarkMode = false;
+}
 
 
     export let shapeInput       : ShapeInput;
@@ -448,17 +462,62 @@
             document.removeEventListener('click', handleClickOutside);
         }
     });
+
+    shapeInput.inputStyle ??= inputUAITheme;
 </script>
 
 
+<!-- class       = { shapeInput.boxSelectClass ?? `${( styles.select as InputStyle ).box }` } -->
 <Info { shapeInput } { onSelectedChange } { value }>
 <div class="relative w-full" bind:this={comboboxElement} >
     <button
         type        = "button"
-        class       = { shapeInput.boxSelectClass ?? `${( styles.select as InputStyle ).box }` }
         on:click    = { handleOpen }
         id          = { shapeInput.id }
         disabled    = { shapeInput.disabled }
+        class       = {`px-3 py-2 transition-all duration-150 ease-in-out w-full flex items-center justify-between 
+            ${ shapeInput.inputStyle?.fontSize      ?? 'text-sm' }
+            ${ shapeInput.inputStyle?.height        ?? '' }
+            ${ shapeInput.inputStyle?.borderRadius  ?? 'rounded-md' }
+            ${ shapeInput.inputStyle?.borderSize    ?? 'border-0' }
+            ${ shapeInput.inputStyle?.boxShadow     ?? 'shadow-sm' }`
+        }
+        style={ `background-color: ${
+            isDarkMode
+                ? shapeInput.disabled || shapeInput.readonly
+                    ? shapeInput.inputStyle?.dark?.event?.disabled?.background ?? 'transparent'
+                    : shapeInput.inputStyle?.dark?.background   ?? 'transparent'
+                : shapeInput.disabled || shapeInput.readonly
+                    ? shapeInput.inputStyle?.light?.event?.disabled?.background ?? 'transparent'
+                    : shapeInput.inputStyle?.light?.background  ?? 'transparent'
+            }; color: ${
+                isDarkMode
+                    ? shapeInput.disabled || shapeInput.readonly
+                        ? shapeInput.inputStyle?.dark?.event?.disabled?.color ?? '#71717a'
+                        :shapeInput.inputStyle?.dark?.color    ?? '#d1d5db'
+                    : shapeInput.disabled || shapeInput.readonly
+                        ? shapeInput.inputStyle?.light?.event?.disabled?.color ?? '#71717a'
+                        :shapeInput.inputStyle?.light?.color   ?? 'black'
+            }; box-shadow: 0 0 0 ${shapeInput.inputStyle?.ringSize ?? '1px'} ${
+                isDarkMode
+                    ? shapeInput.inputStyle?.dark?.ring ?? '#3f3f46'  // zinc-700
+                    : shapeInput.inputStyle?.light?.ring ?? '#d4d4d8' // zinc-300
+            };`
+        }
+        on:focus={(e) => {
+            e.currentTarget.style.boxShadow = `0 0 0 ${shapeInput.inputStyle?.[isDarkMode ? 'dark' : 'light']?.event?.focus?.ringSize ?? '2px'} ${
+                isDarkMode
+                    ? shapeInput.inputStyle?.dark?.event?.focus?.ring ?? '#71717a' // zinc-500
+                    : shapeInput.inputStyle?.light?.event?.focus?.ring ?? '#a1a1aa' // zinc-400
+            }`;
+        }}
+        on:blur={(e) => {
+            e.currentTarget.style.boxShadow = `0 0 0 ${shapeInput.inputStyle?.ringSize ?? '1px'} ${
+                isDarkMode
+                    ? shapeInput.inputStyle?.dark?.ring ?? '#3f3f46' // zinc-500
+                    : shapeInput.inputStyle?.light?.ring ?? '#d4d4d8' // zinc-400
+            }`;
+        }}
     >
         <span class="truncate text-zinc-900 dark:text-zinc-300">
             { displayText }
