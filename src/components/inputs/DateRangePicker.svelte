8b975d6daf5ec6fcd086d5c7fbc7a80ed4eb2fa3
kevinkeyssx <keyss1700@gmail.com>
2025-04-30 20:42:24 -0400

    Box styles implemented

--- src/components/inputs/DateRangePicker.svelte
+++ src/components/inputs/DateRangePicker.svelte
@@ -18,11 +18,26 @@
     import Description                  from "./Description.svelte";
     import VirtualSelect                from "./VirtualSelect.svelte";
     import Input                        from "./Input.svelte";
+    import { theme }            from "$stores";
+    import { inputUAITheme } from "$lib/styles/themes/uai-theme";
 
     export let shapeInput: ShapeInput;
     export let value: DateRange | any | undefined = undefined;
     export let onValueChange: (value: DateRange | undefined) => void;
     export let setError: VoidFunction = () => {};
+
+
+    shapeInput.inputStyle ??= inputUAITheme;
+
+            
+let isDarkMode = $theme === 'dark';
+
+
+$: if ( $theme === 'dark' ) {
+    isDarkMode = true;
+} else {
+    isDarkMode = false;
+}
 
 
     const createEmptyDateRange = () => ({
@@ -202,12 +217,55 @@
 
         <div bind:this={inputContainer} class="relative">
             <!-- Input personalizado -->
+            <!-- class={shapeInput.boxDateClass ?? `${(styles.datepicker as InputStyle).box}`} -->
             <div 
-                class={shapeInput.boxDateClass ?? `${(styles.datepicker as InputStyle).box}`}
                 on:click={togglePopup}
                 role="button"
                 tabindex="0"
                 on:keydown={(e) => e.key === 'Enter' && togglePopup()}
+                class       = {`px-3 py-2 transition-all duration-150 ease-in-out w-full flex items-center justify-between 
+                    ${ shapeInput.inputStyle?.fontSize      ?? 'text-sm' }
+                    ${ shapeInput.inputStyle?.height        ?? '' }
+                    ${ shapeInput.inputStyle?.borderRadius  ?? 'rounded-md' }
+                    ${ shapeInput.inputStyle?.borderSize    ?? 'border-0' }
+                    ${ shapeInput.inputStyle?.boxShadow     ?? 'shadow-sm' }`
+                }
+                style={ `background-color: ${
+                    isDarkMode
+                        ? shapeInput.disabled || shapeInput.readonly
+                            ? shapeInput.inputStyle?.dark?.event?.disabled?.background ?? 'transparent'
+                            : shapeInput.inputStyle?.dark?.background   ?? 'transparent'
+                        : shapeInput.disabled || shapeInput.readonly
+                            ? shapeInput.inputStyle?.light?.event?.disabled?.background ?? 'transparent'
+                            : shapeInput.inputStyle?.light?.background  ?? 'transparent'
+                    }; color: ${
+                        isDarkMode
+                            ? shapeInput.disabled || shapeInput.readonly
+                                ? shapeInput.inputStyle?.dark?.event?.disabled?.color ?? '#71717a'
+                                :shapeInput.inputStyle?.dark?.color    ?? '#d1d5db'
+                            : shapeInput.disabled || shapeInput.readonly
+                                ? shapeInput.inputStyle?.light?.event?.disabled?.color ?? '#71717a'
+                                :shapeInput.inputStyle?.light?.color   ?? 'black'
+                    }; box-shadow: 0 0 0 ${shapeInput.inputStyle?.ringSize ?? '1px'} ${
+                        isDarkMode
+                            ? shapeInput.inputStyle?.dark?.ring ?? '#3f3f46'  // zinc-700
+                            : shapeInput.inputStyle?.light?.ring ?? '#d4d4d8' // zinc-300
+                    };`
+                }
+                on:focus={(e) => {
+                    e.currentTarget.style.boxShadow = `0 0 0 ${shapeInput.inputStyle?.[isDarkMode ? 'dark' : 'light']?.event?.focus?.ringSize ?? '2px'} ${
+                        isDarkMode
+                            ? shapeInput.inputStyle?.dark?.event?.focus?.ring ?? '#71717a' // zinc-500
+                            : shapeInput.inputStyle?.light?.event?.focus?.ring ?? '#a1a1aa' // zinc-400
+                    }`;
+                }}
+                on:blur={(e) => {
+                    e.currentTarget.style.boxShadow = `0 0 0 ${shapeInput.inputStyle?.ringSize ?? '1px'} ${
+                        isDarkMode
+                            ? shapeInput.inputStyle?.dark?.ring ?? '#3f3f46' // zinc-500
+                            : shapeInput.inputStyle?.light?.ring ?? '#d4d4d8' // zinc-400
+                    }`;
+                }}
             >
                 <div class="flex items-center justify-between w-full">
                     <div class="flex-1 flex items-center">
