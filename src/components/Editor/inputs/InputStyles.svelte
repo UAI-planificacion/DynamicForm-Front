508ca351cb4c7b10c67eccba4a16c301241f20b1
kevinkeyssx <keyss1700@gmail.com>
2025-04-30 20:41:37 -0400

    Comments removed

--- src/components/Editor/inputs/InputStyles.svelte
+++ src/components/Editor/inputs/InputStyles.svelte
@@ -12,93 +12,18 @@
     import { VirtualSelect }    from '$components';
     import type { ShapeInput }  from "$models";
     import InputRbgaColors      from './InputRGBAColors.svelte';
+    import { inputUAITheme }    from '$lib/styles/themes/uai-theme';
 
     export let shapeInput: ShapeInput;
 
     let colorTheme: 'light' | 'dark' = 'light';
 
-    shapeInput.inputStyle ??= {}
-    
-    if ( !shapeInput.inputStyle.light  ) {
-        shapeInput.inputStyle.light ??= {}
-
-        shapeInput.inputStyle.light.ringTransparency = '100'
-        shapeInput.inputStyle.light.ringColor = 'currentColor'
-        shapeInput.inputStyle.light.ringTonality = '500'
-        shapeInput.inputStyle.light.ring = 'ring'
-
-    //     shapeInput.inputStyle.light.borderTransparency = '100'
-    // shapeInput.inputStyle.light.borderColor = 'currentColor'
-    // shapeInput.inputStyle.light.borderTonality = '500'
-    // shapeInput.inputStyle.light.border = 'border'
-
-        shapeInput.inputStyle.light.colorTransparency = '100'
-        shapeInput.inputStyle.light.colorColor = 'currentColor'
-        shapeInput.inputStyle.light.colorTonality = '500'
-        shapeInput.inputStyle.light.color = 'color'
-
-        shapeInput.inputStyle.light.backgroundTransparency = '100'
-        shapeInput.inputStyle.light.backgroundColor = 'currentColor'
-        shapeInput.inputStyle.light.backgroundTonality = '500'
-        shapeInput.inputStyle.light.background = 'background'
-
-        shapeInput.inputStyle.light.event ??= {}
-        shapeInput.inputStyle.light.event.focus ??= {}
-
-        shapeInput.inputStyle.light.event.focus.ringColor = 'currentColor'
-        shapeInput.inputStyle.light.event.focus.ringTonality = '500'
-        shapeInput.inputStyle.light.event.focus.ringTransparency = '100'
-        shapeInput.inputStyle.light.event.focus.ring = 'ring'
-        shapeInput.inputStyle.light.event.focus.ringSize = '1px'
-
-    // shapeInput.inputStyle.light.event.focus.borderColor = 'currentColor'
-    // shapeInput.inputStyle.light.event.focus.borderTonality = '500'
-    // shapeInput.inputStyle.light.event.focus.borderTransparency = '100'
-    // shapeInput.inputStyle.light.event.focus.border = 'border'
-    }
-    if ( !shapeInput.inputStyle.dark ) {
-        shapeInput.inputStyle.dark ??= {}
-
-        shapeInput.inputStyle.dark.ringTransparency = '100'
-        shapeInput.inputStyle.dark.ringColor = 'currentColor'
-        shapeInput.inputStyle.dark.ringTonality = '500'
-        shapeInput.inputStyle.dark.ring = 'ring'
-
-    // shapeInput.inputStyle.dark.borderTransparency = '100'
-    // shapeInput.inputStyle.dark.borderColor = 'currentColor'
-    // shapeInput.inputStyle.dark.borderTonality = '500'
-    // shapeInput.inputStyle.dark.border = 'border'
-
-        shapeInput.inputStyle.dark.colorTransparency = '100'
-        shapeInput.inputStyle.dark.colorColor = 'currentColor'
-        shapeInput.inputStyle.dark.colorTonality = '500'
-        shapeInput.inputStyle.dark.color = 'color'
-
-        shapeInput.inputStyle.dark.backgroundTransparency = '100'
-        shapeInput.inputStyle.dark.backgroundColor = 'currentColor'
-        shapeInput.inputStyle.dark.backgroundTonality = '500'
-        shapeInput.inputStyle.dark.background = 'background'
-
-        shapeInput.inputStyle.dark.event ??= {}
-        shapeInput.inputStyle.dark.event.focus ??= {}
-
-        shapeInput.inputStyle.dark.event.focus.ringColor = 'currentColor'
-        shapeInput.inputStyle.dark.event.focus.ringTonality = '500'
-        shapeInput.inputStyle.dark.event.focus.ringTransparency = '100'
-        shapeInput.inputStyle.dark.event.focus.ring = 'ring'
-        shapeInput.inputStyle.dark.event.focus.ringSize = '1px'
-
-    // shapeInput.inputStyle.dark.event.focus.borderColor = 'currentColor'
-    // shapeInput.inputStyle.dark.event.focus.borderTonality = '500'
-    // shapeInput.inputStyle.dark.event.focus.borderTransparency = '100'
-    // shapeInput.inputStyle.dark.event.focus.border = 'border'
-
-    }
+    shapeInput.inputStyle ??= inputUAITheme;
 </script>
 
-<div class="grid grid-cols-1 @lg:grid-cols-2 @2xl:grid-cols-5 gap-2 items-start">
+<div class="grid grid-cols-1 @lg:grid-cols-2 @2xl:grid-cols-5 gap-2 items-start h-8">
     <VirtualSelect
-        value       = { shapeInput.inputStyle?.height?.replace( 'h-', '' )}
+        value       = { shapeInput.inputStyle?.height }
         shapeInput  = {{
             id          : uuid(),
             name        : 'height',
@@ -110,13 +35,12 @@
         }}
         onSelectedChange = {( value ) => {
             if ( value instanceof Array || value === undefined ) return;
-            shapeInput.inputStyle ??= {};
-            shapeInput.inputStyle.height = `h-${ value }`
+            shapeInput.inputStyle!.height = value;
         }}
     />
 
     <VirtualSelect
-        value       = { shapeInput.inputStyle?.fontSize?.replace( 'text-', '' )}
+        value       = { shapeInput.inputStyle?.fontSize }
         shapeInput  = {{
             id                  : uuid(),
             name                : 'fontSize',
@@ -124,18 +48,16 @@
             placeholder         : 'Ingrese el tamaño de texto',
             search              : true,
             searchPlaceholder   : 'Ingrese el tamaño de texto',
-            options             : BASIC_SIZE
+            options             : BASIC_SIZE('text')
         }}
         onSelectedChange = {( value ) => {
             if ( value instanceof Array || value === undefined ) return;
-            shapeInput.inputStyle ??= {};
-            shapeInput.inputStyle.fontSize = `text-${ value }`
+            shapeInput.inputStyle!.fontSize = value;
         }}
     />
 
-
     <VirtualSelect
-        value       = { shapeInput.inputStyle?.borderRadius?.replace( 'rounded-', '' )}
+        value       = { shapeInput.inputStyle?.borderRadius }
         shapeInput  = {{
             id                  : uuid(),
             name                : 'rounded',
@@ -143,17 +65,16 @@
             placeholder         : 'Ingrese el redondeado',
             search              : true,
             searchPlaceholder   : 'Ingrese el redondeado',
-            options             : SIZE
+            options             : SIZE('rounded')
         }}
         onSelectedChange = {( value ) => {
             if ( value instanceof Array || value === undefined ) return;
-            shapeInput.inputStyle ??= {};
-            shapeInput.inputStyle.borderRadius = `rounded-${ value }`
+            shapeInput.inputStyle!.borderRadius = value;
         }}
     />
 
     <VirtualSelect
-        value       = { shapeInput.inputStyle?.boxShadow?.replace( 'shadow-', '' )}
+        value       = { shapeInput.inputStyle?.boxShadow}
         shapeInput  = {{
             id                  : uuid(),
             name                : 'box-shadow',
@@ -165,28 +86,9 @@
         }}
         onSelectedChange = {( value ) => {
             if ( value instanceof Array || value === undefined ) return;
-            shapeInput.inputStyle ??= {};
-            shapeInput.inputStyle.boxShadow = `shadow-${ value }`
+            shapeInput.inputStyle!.boxShadow = value;
         }}
     />
-
-    <!-- <VirtualSelect
-        value       = { shapeInput.inputStyle?.borderSize?.replace( 'border-', '' )}
-        shapeInput  = {{
-            id                  : uuid(),
-            name                : 'border',
-            label               : 'Tamaño',
-            placeholder         : 'Ingrese el tamaño',
-            search              : true,
-            searchPlaceholder   : 'Ingrese el tamaño',
-            options             : BORDER
-        }}
-        onSelectedChange = {( value ) => {
-            if ( value instanceof Array || value === undefined ) return;
-            shapeInput.inputStyle ??= {};
-            shapeInput.inputStyle.borderSize = `border-${ value }`
-        }}
-    /> -->
 
     <VirtualSelect
         value       = { shapeInput.inputStyle?.ringSize}
@@ -201,9 +103,7 @@
         }}
         onSelectedChange = {( value ) => {
             if ( value instanceof Array || value === undefined ) return;
-            shapeInput.inputStyle ??= {};
-            shapeInput.inputStyle.ringSize = value
-            console.log('🚀 ~ file: InputStyles.svelte:129 ~ shapeInput.inputStyle.ringSize:', shapeInput.inputStyle.ringSize)
+            shapeInput.inputStyle!.ringSize = value;
         }}
     />
 </div>
@@ -230,76 +130,50 @@
     </Tabs.List>
 </Tabs.Root>
 
-<!-- <div class="grid"> -->
-    <InputRbgaColors
-        name = {"Fondo"}
-        bind:color={ shapeInput.inputStyle![colorTheme]!.backgroundColor as string }
-        bind:tonality={ shapeInput.inputStyle![colorTheme]!.backgroundTonality as string }
-        bind:transparency={ shapeInput.inputStyle![colorTheme]!.backgroundTransparency as string }
-        bind:type={ shapeInput.inputStyle![colorTheme]!.background as string}
-    />
-
-    <Separator.Root
-        orientation = "horizontal"
-        class       = "w-full h-[1px] rounded-lg bg-primary dark:bg-zinc-700 my-4 @xl:my-1"
-    />
-
-    <InputRbgaColors
-        name = {"Texto"}
-        bind:color={ shapeInput.inputStyle![colorTheme]!.colorColor as string }
-        bind:tonality={ shapeInput.inputStyle![colorTheme]!.colorTonality as string }
-        bind:transparency={ shapeInput.inputStyle![colorTheme]!.colorTransparency as string }
-        bind:type={ shapeInput.inputStyle![colorTheme]!.color as string}
-    />
-
-    <Separator.Root
-        orientation = "horizontal"
-        class       = "w-full h-[1px] rounded-lg bg-primary dark:bg-zinc-700 my-4 @xl:my-1"
-    />
-
-    <!-- <InputRbgaColors
-        name = {"Borde"}
-        bind:color={ shapeInput.inputStyle![colorTheme]!.borderColor as string }
-        bind:tonality={ shapeInput.inputStyle![colorTheme]!.borderTonality as string }
-        bind:transparency={ shapeInput.inputStyle![colorTheme]!.borderTransparency as string }
-        bind:type={ shapeInput.inputStyle![colorTheme]!.border as string}
-    /> -->
-
-    <Separator.Root
-        orientation = "horizontal"
-        class       = "w-full h-[1px] rounded-lg bg-primary dark:bg-zinc-700 my-4 @xl:my-1"
-    />
-
-    <InputRbgaColors
-        name = "Anillo"
-        bind:color={ shapeInput.inputStyle![colorTheme]!.ringColor as string }
-        bind:tonality={ shapeInput.inputStyle![colorTheme]!.ringTonality as string }
-        bind:transparency={ shapeInput.inputStyle![colorTheme]!.ringTransparency as string }
-        bind:type={ shapeInput.inputStyle![colorTheme]!.ring as string}
-    />
-<!-- </div> -->
+<InputRbgaColors
+    name = {"Fondo"}
+    bind:color={ shapeInput.inputStyle![colorTheme]!.backgroundColor as string }
+    bind:tonality={ shapeInput.inputStyle![colorTheme]!.backgroundTonality as string }
+    bind:transparency={ shapeInput.inputStyle![colorTheme]!.backgroundTransparency as string }
+    bind:type={ shapeInput.inputStyle![colorTheme]!.background as string}
+/>
 
 <Separator.Root
-        orientation = "horizontal"
-        class       = "w-full h-[1px] rounded-lg bg-primary dark:bg-zinc-700 my-4 @xl:my-1"
-    />
+    orientation = "horizontal"
+    class       = "w-full h-[1px] rounded-lg bg-primary dark:bg-zinc-700 my-4 @xl:my-1"
+/>
 
+<InputRbgaColors
+    name = {"Texto"}
+    bind:color={ shapeInput.inputStyle![colorTheme]!.colorColor as string }
+    bind:tonality={ shapeInput.inputStyle![colorTheme]!.colorTonality as string }
+    bind:transparency={ shapeInput.inputStyle![colorTheme]!.colorTransparency as string }
+    bind:type={ shapeInput.inputStyle![colorTheme]!.color as string}
+/>
 
-    <InputRbgaColors
-        name = "Foco Borde"
-        bind:color={ shapeInput.inputStyle![colorTheme]!.event!.focus!.ringColor as string }
-        bind:tonality={ shapeInput.inputStyle![colorTheme]!.event!.focus!.ringTonality as string }
-        bind:transparency={ shapeInput.inputStyle![colorTheme]!.event!.focus!.ringTransparency as string }
-        bind:type={ shapeInput.inputStyle![colorTheme]!.event!.focus!.ring as string}
-        bind:size={ shapeInput.inputStyle![colorTheme]!.event!.focus!.ringSize as string}
+<Separator.Root
+    orientation = "horizontal"
+    class       = "w-full h-[1px] rounded-lg bg-primary dark:bg-zinc-700 my-4 @xl:my-1"
+/>
 
-    />
-    <!-- <InputRbgaColors
-        name = "Focus Border"
-        bind:color={ shapeInput.inputStyle![colorTheme]!.event!.focus!.borderColor as string }
-        bind:tonality={ shapeInput.inputStyle![colorTheme]!.event!.focus!.borderTonality as string }
-        bind:transparency={ shapeInput.inputStyle![colorTheme]!.event!.focus!.borderTransparency as string }
-        bind:type={ shapeInput.inputStyle![colorTheme]!.event!.focus!.border as string}
-    /> -->
+<InputRbgaColors
+    name = "Anillo"
+    bind:color={ shapeInput.inputStyle![colorTheme]!.ringColor as string }
+    bind:tonality={ shapeInput.inputStyle![colorTheme]!.ringTonality as string }
+    bind:transparency={ shapeInput.inputStyle![colorTheme]!.ringTransparency as string }
+    bind:type={ shapeInput.inputStyle![colorTheme]!.ring as string}
+/>
 
-<!-- TODO: hay que crear los eventos para :hover, :focus, :disabled -->
+<Separator.Root
+    orientation = "horizontal"
+    class       = "w-full h-[1px] rounded-lg bg-primary dark:bg-zinc-700 my-4 @xl:my-1"
+/>
+
+<InputRbgaColors
+    name = "Foco Borde"
+    bind:color={ shapeInput.inputStyle![colorTheme]!.event!.focus!.ringColor as string }
+    bind:tonality={ shapeInput.inputStyle![colorTheme]!.event!.focus!.ringTonality as string }
+    bind:transparency={ shapeInput.inputStyle![colorTheme]!.event!.focus!.ringTransparency as string }
+    bind:type={ shapeInput.inputStyle![colorTheme]!.event!.focus!.ring as string}
+    bind:size={ shapeInput.inputStyle![colorTheme]!.event!.focus!.ringSize as string}
+/>
