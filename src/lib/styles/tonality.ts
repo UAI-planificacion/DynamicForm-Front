568e0d369e431399ba459861a3978196be17a8b6
kevinkeyssx <keyss1700@gmail.com>
2025-05-05 23:33:30 -0400

    50 tonality added

--- src/lib/styles/tonality.ts
+++ src/lib/styles/tonality.ts
@@ -1,10 +1,13 @@
 import type { ShapeOption } from "$models";
 
 
-export const TONALITY: ShapeOption[] = Array.from({ length: 9 }, (_, i) => {
-    const value = ((i + 1) * 100).toString();
-    return {
-        label: value,
-        value: value
-    };
-});
+export const TONALITY: ShapeOption[] = [
+    {label: '50', value: '50'},
+    ...Array.from({ length: 9 }, (_, i) => {
+        const value = ((i + 1) * 100).toString();
+        return {
+            label: value,
+            value: value
+        };
+    })
+];
