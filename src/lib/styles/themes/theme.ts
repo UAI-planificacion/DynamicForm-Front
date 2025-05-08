fde448b8c7cd2ea9e33cde6e82080d7a8fadf2c7
kevinkeyssx <keyss1700@gmail.com>
2025-05-07 22:53:09 -0400

    Theme ShapeOptions added

---
+++ src/lib/styles/themes/theme.ts
@@ -0,0 +1,10 @@
+import type { ShapeOption } from "$models";
+
+export const THEME: ShapeOption[] = [
+    { label: "UAI",         value: "uai" },
+    { label: "Elegante",    value: "elegant" },
+    { label: "Casual",    value: "casual" },
+    { label: "Marked",      value: "marked" },
+    { label: "Neutral",     value: "neutral" },
+    { label: "Minimalista", value: "minimal" },
+];
