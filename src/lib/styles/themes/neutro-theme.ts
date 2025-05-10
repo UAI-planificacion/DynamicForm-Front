d3c3e5550914d1606898395b59b5ce56cee2cbb3
kevinkeyssx <keyss1700@gmail.com>
2025-05-09 20:47:33 -0400

    Neutro theme updated

--- src/lib/styles/themes/neutro-theme.ts
+++ src/lib/styles/themes/neutro-theme.ts
@@ -1,64 +1,64 @@
-import type { ShapeInputStyle } from "$models";
+import type { ThemeShape } from "$models";
 import { tailwindToRGBA } from "../tailwind-colors";
 
-export const neutralTheme = ( color: string = 'slate' ): ShapeInputStyle => ({
-
+export const neutralTheme = ( color: string = 'slate' ): ThemeShape => ({
     height          : 'h-9',
     fontSize        : 'text-sm',
-    borderRadius    : 'rounded-md',
-    boxShadow       : 'shadow-sm',
-    ringSize        : '1px',
+    borderRadius    : 'rounded-full',
+    boxShadow       : 'shadow-xl',
+    ringSize        : '0px',
+    borderSize      : 'border-0',
 
     light : {
-        backgroundTransparency : '100',
-        backgroundColor        : color,
-        backgroundTonality     : '50',
-        background             : tailwindToRGBA(`${color}-50`),
+        backgroundOpacity   : '100',
+        backgroundTonality  : '50',
+        backgroundColor     : color,
+        background          : tailwindToRGBA(`${color}-50`),
 
-        colorTransparency      : '100',
-        colorTonality          : '700',
-        colorColor             : color,
-        color                  : tailwindToRGBA(`${color}-700`),
+        colorOpacity    : '100',
+        colorTonality   : '700',
+        colorColor      : color,
+        color           : tailwindToRGBA(`${color}-700`),
 
-        ringTransparency       : '100',
-        ringColor              : color,
-        ringTonality           : '200',
-        ring                   : tailwindToRGBA(`${color}-200`),
+        ringOpacity     : '100',
+        ringTonality    : '200',
+        ringColor       : color,
+        ring            : tailwindToRGBA(`${color}-200`),
 
         event : {
             focus : {
-                ringColor        : color,
-                ringTonality     : '400',
-                ringTransparency : '100',
-                ring             : tailwindToRGBA(`${color}-400`),
-                ringSize         : '2px',
+                ringTonality    : '400',
+                ringOpacity     : '100',
+                ringColor       : color,
+                ring            : tailwindToRGBA(`${color}-400`),
+                ringSize        : '1px',
             }
         }
     },
 
     dark : {
-        backgroundTransparency : '100',
-        backgroundColor        : color,
-        backgroundTonality     : '800',
-        background             : tailwindToRGBA(`${color}-800`),
+        backgroundOpacity   : '100',
+        backgroundTonality  : '800',
+        backgroundColor     : color,
+        background          : tailwindToRGBA(`${color}-800`),
 
-        colorTransparency      : '100',
-        colorTonality          : '200',
-        colorColor             : color,
-        color                  : tailwindToRGBA(`${color}-200`),
+        colorOpacity    : '100',
+        colorTonality   : '200',
+        colorColor      : color,
+        color           : tailwindToRGBA(`${color}-200`),
 
-        ringTransparency       : '100',
-        ringColor              : color,
-        ringTonality           : '500',
-        ring                   : tailwindToRGBA(`${color}-500`),
+        ringOpacity     : '100',
+        ringTonality    : '500',
+        ringColor       : color,
+        ring            : tailwindToRGBA(`${color}-500`),
 
         event : {
             focus : {
-                ringColor        : color,
-                ringTonality     : '400',
-                ringTransparency : '100',
-                ring             : tailwindToRGBA(`${color}-400`),
-                ringSize         : '2px',
+                ringTonality    : '400',
+                ringOpacity     : '100',
+                ringColor       : color,
+                ring            : tailwindToRGBA(`${color}-400`),
+                ringSize        : '1px',
             }
         }
     }
