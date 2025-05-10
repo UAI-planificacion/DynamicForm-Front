6f7710d146fe41080c9385bf0c5a5d9286ebcea7
kevinkeyssx <keyss1700@gmail.com>
2025-05-09 20:39:50 -0400

    UAITheme added by default & syles & focus & blur methos implemented

--- src/components/inputs/Check.svelte
+++ src/components/inputs/Check.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
     import { theme }            from "$stores";
     import { CheckIcon }        from "$icons";
-    import type { ShapeInput }  from "$models";
+    import type { ShapeInput, ThemeShape }  from "$models";
     import Description          from "./Description.svelte";
+    import { UAITheme }         from "$lib/styles/themes";
+    import { style }            from "$lib/styles/themes/default/style";
+    import { focus }            from "$lib/styles/themes/default/focus";
+    import { blur }             from "$lib/styles/themes/default/blur";
 
 
     let isDarkMode = $theme === 'dark';
@@ -19,6 +23,7 @@
     export let onChange     : ( checked: boolean ) => void;
     export let checked      : boolean | undefined = undefined;
     export let setError     : VoidFunction = () => {};
+    export let themeShape   : ThemeShape = UAITheme();
 
 
     shapeInput.checked = checked ?? false;
@@ -40,61 +45,18 @@
             type        = "button"
             disabled    = { shapeInput.disabled || shapeInput.readonly }
             class       = {`
-                inline-flex items-center transition-all duration-150 ease-in-out 
-                ${shapeInput.inputStyle?.borderRadius ?? 'rounded-md'}
-                ${shapeInput.inputStyle?.borderSize ?? 'border-0'}
+                relative inline-flex items-center transition-all duration-150 ease-in-out 
                 h-6 w-6
-                ${shapeInput.disabled || shapeInput.readonly ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer'}
-                relative
+                ${themeShape.borderRadius }
+                ${themeShape.borderSize }
+                ${shapeInput.disabled || shapeInput.readonly
+                    ? 'opacity-50 cursor-not-allowed'
+                    : 'cursor-pointer'
+                }
             `}
-            style={`
-                background-color: ${
-                isDarkMode
-                    ? shapeInput.disabled || shapeInput.readonly
-                    ? shapeInput.inputStyle?.dark?.event?.disabled?.background ?? 'transparent'
-                    : shapeInput.inputStyle?.dark?.background ?? 'transparent'
-                    : shapeInput.disabled || shapeInput.readonly
-                    ? shapeInput.inputStyle?.light?.event?.disabled?.background ?? 'transparent'
-                    : shapeInput.inputStyle?.light?.background ?? 'transparent'
-                };
-                box-shadow: 0 0 0 ${shapeInput.inputStyle?.ringSize ?? '1px'} ${
-                isDarkMode
-                    ? shapeInput.inputStyle?.dark?.ring ?? '#3f3f46'
-                    : shapeInput.inputStyle?.light?.ring ?? '#d4d4d8'
-                };
-                border-color: ${
-                isDarkMode
-                    ? shapeInput.inputStyle?.dark?.border ?? '#3f3f46'
-                    : shapeInput.inputStyle?.light?.border ?? '#d4d4d8'
-                };
-            `}
-            on:focus={(e) => {
-                e.currentTarget.style.boxShadow = `0 0 0 ${shapeInput.inputStyle?.[isDarkMode ? 'dark' : 'light']?.event?.focus?.ringSize ?? '2px'} ${
-                    isDarkMode
-                        ? shapeInput.inputStyle?.dark?.event?.focus?.ring ?? '#71717a' // zinc-500
-                        : shapeInput.inputStyle?.light?.event?.focus?.ring ?? '#a1a1aa' // zinc-400
-                }`;
-            }}
-            on:blur={(e) => {
-                e.currentTarget.style.boxShadow = `0 0 0 ${shapeInput.inputStyle?.ringSize ?? '1px'} ${
-                    isDarkMode
-                        ? shapeInput.inputStyle?.dark?.ring ?? '#3f3f46' // zinc-500
-                        : shapeInput.inputStyle?.light?.ring ?? '#d4d4d8' // zinc-400
-                }`;
-            }}
-            on:focus={(e) => {
-                e.currentTarget.style.borderColor = `${
-                    isDarkMode
-                        ? shapeInput.inputStyle?.dark?.event?.focus?.border ?? '#71717a' // zinc-500
-                        : shapeInput.inputStyle?.light?.event?.focus?.border ?? '#a1a1aa' // zinc-400
-            }`;
-            }}
-            on:blur={(e) => {
-                e.currentTarget.style.borderColor = `${
-                    isDarkMode ?
-                shapeInput.inputStyle?.dark?.border
-                : shapeInput.inputStyle?.light?.border}`;
-            }}
+            style       = {style( isDarkMode, themeShape )}
+            on:focus    = {( e ) => focus( e, isDarkMode, themeShape )}
+            on:blur     = {( e ) => blur( e, isDarkMode, themeShape )}
         >
             {#if shapeInput.checked}
                 <div class="absolute inset-0 flex items-center justify-center">
