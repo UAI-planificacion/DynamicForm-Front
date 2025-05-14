c8bfa6033cdd4a59955c26a2ee500136033ca113
kevinkeyssx <keyss1700@gmail.com>
2025-05-14 19:11:13 -0400

    CurrentColor updated

--- src/icons/markdown/Heading1Icon.svelte
+++ src/icons/markdown/Heading1Icon.svelte
@@ -1,1 +1,34 @@
-<svg width="18px" height="18px" viewBox="0 -2 20 20" xmlns="http://www.w3.org/2000/svg" fill="#000000" data-darkreader-inline-fill="" style="--darkreader-inline-fill: #000000;"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="heading-1" transform="translate(-2 -4)"> <path id="primary" d="M19,19V9l-2,1" fill="none" stroke="#ffffff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" data-darkreader-inline-stroke="" style="--darkreader-inline-stroke: #e8e6e3;"></path> <path id="primary-2" data-name="primary" d="M17,19h4M4,5V19M5,5H3M5,19H3M12,5V19M11,5h2M11,19h2M4,12h8" fill="none" stroke="#ffffff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" data-darkreader-inline-stroke="" style="--darkreader-inline-stroke: #e8e6e3;"></path> </g> </g></svg>
+<svg
+  width="18px"
+  height="18px"
+  viewBox="0 -2 20 20"
+  xmlns="http://www.w3.org/2000/svg"
+  fill="currentColor"
+  ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g
+    id="SVGRepo_tracerCarrier"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  ></g><g id="SVGRepo_iconCarrier">
+    <g id="heading-1" transform="translate(-2 -4)">
+      <path
+        id="primary"
+        d="M19,19V9l-2,1"
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+      ></path>
+      <path
+        id="primary-2"
+        data-name="primary"
+        d="M17,19h4M4,5V19M5,5H3M5,19H3M12,5V19M11,5h2M11,19h2M4,12h8"
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+      ></path>
+    </g>
+  </g></svg
+>
