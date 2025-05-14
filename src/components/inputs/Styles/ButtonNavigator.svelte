495fa7dc5f5e8f4153d3b97d360fb5f0ec85aa56
kevinkeyssx <keyss1700@gmail.com>
2025-05-13 21:36:02 -0400

    Select, select defaul, hover updated

--- src/components/inputs/Styles/ButtonNavigator.svelte
+++ src/components/inputs/Styles/ButtonNavigator.svelte
@@ -1,25 +1,32 @@
 
 <script lang="ts">
-    import { tailwindToRGBA } from "$lib/styles";
-    import type { ThemeShape } from "$models";
+    import { tailwindToRGBA }   from "$lib/styles";
+    import type { ThemeShape }  from "$models";
     import { theme }            from "$stores";
 
 
-    export let themeShape   : ThemeShape;
-    export let onClick      : ( event: MouseEvent ) => void;
-    export let className    : string = '';
-    export let selected     : boolean = false;
-    export let canHover     : boolean = true;
-    export let disabled     : boolean = false;
-    export let style        : string = '';
-    export let defaultSelected : boolean= false;
-    export let rounded : string | null = null;
+    type Colors = {
+        tonalityDark : number,
+        tonalityLight : number,
+        opacity  : number
+    }
 
-    export let tonalityLight : number = 100;
-    export let tonalityDark : number = 100;
+
+    export let themeShape           : ThemeShape;
+    export let onClick              : ( event: MouseEvent ) => void;
+    export let className            : string = '';
+    export let selected             : boolean = false;
+    export let canHover             : boolean = true;
+    export let disabled             : boolean = false;
+    export let style                : string = '';
+    export let defaultSelected      : boolean = false;
+    export let rounded              : string | null = null;
+    export let colorHover           : Colors = { tonalityDark: 100, tonalityLight: 200, opacity: 60 };
+    export let colorSelected        : Colors = { tonalityDark: 100, tonalityLight: 200, opacity: 100 };
+    export let colorDefaultSelected : Colors = { tonalityDark: 100, tonalityLight: 200, opacity: 100 };
+
 
     let isHovered = false;
-
     let isDarkMode = $theme === 'dark';
 
 
@@ -30,29 +37,43 @@
     }
 
 
-    function setTonalityDark( theme: ThemeShape, tonalityRange: number, opacity: number ) {
-        const tonalityNumber = Number( theme.dark.backgroundTonality );
+    function setTonalityDark(
+        theme: ThemeShape,
+        tonalityRange: number,
+        opacity: number
+    ): string {
+        let tonalityNumber = Number( theme.dark.backgroundTonality );
+
+        if ( tonalityNumber === 950 ) tonalityNumber = 900;
+
         let tonality = tonalityNumber - tonalityRange;
 
-        if ( tonalityNumber === 950 ) tonality = 900;
-
         return `bg-${theme.dark.backgroundColor}-${tonality}/${opacity}`;
-
     }   
 
-    function setTonalityLight( theme: ThemeShape, tonalityRange: number, opacity: number ) {
-        const tonalityNumber = Number( theme.light.backgroundTonality );
-        let tonality = tonalityNumber + tonalityRange;
 
-        if ( tonalityNumber === 50 ) tonality = 200;
+    function setTonalityLight(
+        theme: ThemeShape,
+        tonalityRange: number,
+        opacity: number
+    ): string {
+        let tonalityNumber = Number( theme.light.backgroundTonality );
+
+        if ( tonalityNumber === 50 ) tonalityNumber = 100;
+
+        let tonality = tonalityNumber + tonalityRange;
 
         return `bg-${theme.light.backgroundColor}-${tonality}/${opacity}`;
     }
 
-    function setTonality( theme: ThemeShape, opacity: number) {
+
+    function setTonality(
+        theme: ThemeShape,
+        colors: Colors
+    ): string {
         const color = isDarkMode
-            ? setTonalityDark( theme, tonalityDark, opacity )
-            : setTonalityLight( theme, tonalityLight, opacity );
+            ? setTonalityDark( theme, colors.tonalityDark, colors.opacity )
+            : setTonalityLight( theme, colors.tonalityLight, colors.opacity );
 
         return tailwindToRGBA( color );
     }
@@ -65,23 +86,26 @@
     on:mouseenter   = {() => isHovered = canHover }
     on:mouseleave   = {() => isHovered = false }
     class           = {`
-        ${ rounded ? rounded : themeShape.borderRadius }
+        ${ rounded ?? themeShape.borderRadius }
         ${ themeShape.fontSize }
         ${ className }
         flex
         items-center
-        transition-all
-        duration-200
-        ${ disabled ? 'cursor-not-allowed opacity-50' : '' }
+        transition-transform
+        duration-100
+        disabled:cursor-not-allowed
+        disabled:opacity-50
+        disabled:scale-100
+        disabled:shadow-none
     `}
-    style= {`
+    style = {`
         background-color:
         ${ isHovered
-            ? setTonality( themeShape, 60 )
+            ? setTonality( themeShape, colorHover )
             : selected
-                ? setTonality( themeShape, 90)
+                ? setTonality( themeShape, colorSelected )
                 : defaultSelected
-                    ? setTonality( themeShape, 100 )
+                    ? setTonality( themeShape, colorDefaultSelected )
                     : ''
         };
         ${style}
