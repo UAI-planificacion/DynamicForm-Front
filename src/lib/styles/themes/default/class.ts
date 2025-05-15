160a4ffbb01d653a5f47b8b3cab6f0eb041dd806
kevinkeyssx <keyss1700@gmail.com>
2025-05-15 17:46:16 -0400

    IsRadius usen in textarea className

--- src/lib/styles/themes/default/class.ts
+++ src/lib/styles/themes/default/class.ts
@@ -5,12 +5,13 @@ export const className = (
     isBox       : boolean,
     themeShape  : ThemeShape,
     height      : boolean = true,
+    isRadius    : boolean = true
 ): string => (
     `px-3 py-2 w-full transition-all duration-200 ease-in-out disabled:cursor-not-allowed disabled:opacity-50
     ${ isBox ? 'flex items-center justify-between' : '' } 
     ${ themeShape.fontSize }
     ${ height ? themeShape.height : 'h-auto' }
-    ${ themeShape.borderRadius }
+    ${ !isRadius && themeShape.borderRadius === 'rounded-full' ? 'rounded-2xl': themeShape.borderRadius }
     ${ themeShape.borderSize }
     ${ themeShape.boxShadow }
 `);
