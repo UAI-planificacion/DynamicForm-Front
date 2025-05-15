6b6585620e9a483283249038d21b6a2b11acd364
kevinkeyssx <keyss1700@gmail.com>
2025-05-15 17:45:39 -0400

    Fixed rounded full in textarea now max is rounded-2xl

--- src/components/inputs/TextArea.svelte
+++ src/components/inputs/TextArea.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
     import type { ShapeInput, ThemeShape } 	from "$models";
-    import { theme }            from "$stores";
-    import { style }    		from "$lib/styles/themes/default/style";
-    import { focus }    		from "$lib/styles/themes/default/focus";
-    import { blur }     		from "$lib/styles/themes/default/blur";
-    import { className } 		from "$lib/styles/themes/default/class";
-	import Info 				from "./Info.svelte";
-    import { UAITheme } from "$lib";
+    import { theme }                        from "$stores";
+    import { style }    		            from "$lib/styles/themes/default/style";
+    import { focus }    		            from "$lib/styles/themes/default/focus";
+    import { blur }     		            from "$lib/styles/themes/default/blur";
+    import { className } 		            from "$lib/styles/themes/default/class";
+	import Info 				            from "./Info.svelte";
+    import { UAITheme }                     from "$lib";
 
 
     export let shapeInput	: ShapeInput;
@@ -39,7 +39,7 @@
 		disabled 	= { shapeInput.disabled }
 		placeholder	= { shapeInput.placeholder }
 		on:input    = { ( event: Event ) => { onInput(( event.target as HTMLTextAreaElement ).value ); setError() }}
-        class       = { className( false, themeShape, false )}
+        class       = { className( false, themeShape, false, false )}
         style       = { style( isDarkMode, themeShape )}
         on:focus    = {( e ) => focus( e, isDarkMode, themeShape )}
         on:blur     = {( e ) => blur( e, isDarkMode, themeShape )}
