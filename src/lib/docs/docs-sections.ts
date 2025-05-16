c7fafa9df48faef60909771514f9660758087b36
kevinkeyssx <keyss1700@gmail.com>
2025-05-15 21:44:51 -0400

    Theme doc in sections

--- src/lib/docs/docs-sections.ts
+++ src/lib/docs/docs-sections.ts
@@ -9,6 +9,7 @@ import { DATE_SHAPE }       from "$lib/docs/shape-inputs/date";
 import { MARKDOWN_SHAPE }   from "$lib/docs/shape-inputs/markdown";
 import { TIME_SHAPE }       from "$lib/docs/shape-inputs/time";
 import { BUTTON_SHAPE }     from "$lib/docs/shape-inputs/button";
+import { THEME }            from "./theme/theme-doc";
 
 
 export const docSections: DocSection[] = [
@@ -73,6 +74,18 @@ export const docSections: DocSection[] = [
                 id: 'button',
                 title: 'Botón',
                 component: BUTTON_SHAPE
+            }
+        ]
+    },
+    {
+        id: 'themes',
+        title: 'Temas',
+        icon: 'M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253',
+        items: [
+            {
+                id: 'customization',
+                title: 'Customización',
+                component: THEME
             }
         ]
     }
